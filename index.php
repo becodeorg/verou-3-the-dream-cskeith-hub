7fbeb299db713203a4924d3aba9efc84a5a552b3
unknown <cskeith01@gmail.com>
2022-02-25 10:56:21 +0100

    Add converters

--- index.php
+++ index.php
@@ -13,28 +13,28 @@ function pre_r( $array )  // Super Gobal Variable ARRAY
 
 pre_r($_POST); // Calling SuperGlobal  $_POST
 
-$idr = "16.103,73"; // rate of 25-2-2022 to 1EURO
+$idr = 16103.73; // rate of 25-2-2022 to 1EURO
 
-$eur = 1;
+$eur = 0.000062; // rate of 25-2-2022 to 1 idr
 
-echo $idr;
 
-echo $eur;
 
 if(isset($_POST['submit'])){ // in Use of submit
 
-    fromToCheck(); // start this function
+    fromToCheck($idr,$eur); // start this function
 
 }
 
-function fromToCheck(){ // function that checks From / To
+function fromToCheck($idr,$eur){ // function that checks From / To
     if ($_POST['valuta1'] =="EUR" AND $_POST['valuta2'] == "IDR"){
         
         echo ($_POST["amountInput"]." ". $_POST["valuta1"] );
 
         echo " Is worth ";
 
-        echo ($_POST["valuta2"]);
+        echo ($_POST['amountInput']  * $idr);
+
+        echo " ".($_POST["valuta2"]);
 
     } else if($_POST['valuta1'] =="IDR" AND $_POST['valuta2'] == "EUR"){
 
@@ -42,7 +42,9 @@ function fromToCheck(){ // function that checks From / To
 
         echo " Is worth ";
 
-        echo ($_POST["valuta2"]);
+        echo ($_POST['amountInput'] * $eur);
+
+        echo " ".($_POST["valuta2"]);
     }
 }
 
